16439c55337cbb2d3d99a9ebc4afe946a0eb03d8
LucasL53 <57372991+LucasL53@users.noreply.github.com>
2022-10-22 14:53:22 -0700

    Update three.js

--- three.js
+++ three.js
@@ -1,4 +1,7 @@
-<script>
+"use strict";
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
     // Our Javascript will go here.
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -23,4 +26,5 @@
         renderer.render( scene, camera );
     }
     animate();
-</script>
+
+}
